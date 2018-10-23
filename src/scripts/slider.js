c22ce26df5839d99b30d8f16b29b5a7db98ece39
Anshul Sahni <anshul_sahni@live.com>
2018-10-24 00:23:48 +0530

    removed slider.js from scripts folder

--- src/scripts/slider.js
+++
@@ -1,7 +0,0 @@
-// const $next = document.querySelector('.next');
-// const $prev = document.querySelector('.prev');
-
-function changeImage(counter, gallerySlider) {
-  return images = document.querySelectorAll(gallerySlider + ' .images');
-  // console.log({images});
-}
