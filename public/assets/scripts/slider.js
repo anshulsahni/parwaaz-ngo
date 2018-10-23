90582267b9d9f793463a74376c106ed3fb048f44
Anshul Sahni <anshul_sahni@live.com>
2018-10-24 00:29:39 +0530

    [FIX]: fixed slider.js script

--- public/assets/scripts/slider.js
+++ public/assets/scripts/slider.js
@@ -5,8 +5,8 @@ const $event_1_prevButton = document.querySelector('#event_1 .prev');
 const $event_2_nextButton = document.querySelector('#event_2 .next');
 const $event_2_prevButton = document.querySelector('#event_2 .prev');
 
-$event_1_nextButton.addEventListener('click', changeImage.bind(null, 1, '#event_1'));
-$event_1_prevButton.addEventListener('click', changeImage.bind(null, -1, '#event_1'));
+// $event_1_nextButton.addEventListener('click', changeImage.bind(null, 1, '#event_1'));
+// $event_1_prevButton.addEventListener('click', changeImage.bind(null, -1, '#event_1'));
 
 $event_2_nextButton.addEventListener('click', changeImage.bind(null, 1, '#event_2'));
 $event_2_prevButton.addEventListener('click', changeImage.bind(null, -1, '#event_2'));
@@ -19,9 +19,9 @@ function changeImage(counter, gallerySlider) {
     if (notDone && image.classList.contains('active')) {
       notDone = false;
       image.classList.remove('active');
-      if (index === images.length - 1) {
+      if (counter > 0 && index === images.length - 1) {
         images[0].classList.add('active');
-      } else if (index === 0) {
+      } else if (counter < 0 && index === 0) {
         images[images.length - 1].classList.add('active');
       } else {
         images[index + (counter)].classList.add('active');
