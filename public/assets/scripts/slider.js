a9cdb5af90b4115686aef1ca4fae4daa653218eb
Anshul Sahni <anshul_sahni@live.com>
2018-10-25 02:22:58 +0530

    Fixed gallery event capture

--- public/assets/scripts/slider.js
+++ public/assets/scripts/slider.js
@@ -5,11 +5,8 @@ const $event_1_prevButton = document.querySelector('#event_1 .prev');
 const $event_2_nextButton = document.querySelector('#event_2 .next');
 const $event_2_prevButton = document.querySelector('#event_2 .prev');
 
-// $event_1_nextButton.addEventListener('click', changeImage.bind(null, 1, '#event_1'));
-// $event_1_prevButton.addEventListener('click', changeImage.bind(null, -1, '#event_1'));
-
-$event_2_nextButton.addEventListener('click', changeImage.bind(null, 1, '#event_2'));
-$event_2_prevButton.addEventListener('click', changeImage.bind(null, -1, '#event_2'));
+$event_1_nextButton.addEventListener('click', changeImage.bind(null, 1, '#event_1'));
+$event_1_prevButton.addEventListener('click', changeImage.bind(null, -1, '#event_1'));
 
 function changeImage(counter, gallerySlider) {
   const images = document.querySelectorAll(gallerySlider + ' .images');
